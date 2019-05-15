a55d3965a5f3738feb43be0d2f40e35c85b07b7e
Nitish Kumar <nitish.kumar@tarento.com>
2019-05-16 00:16:46 +0530

    Models

--- SwiftPractice1/SwiftPractice1/Controller/LeagueVC.swift
+++ SwiftPractice1/SwiftPractice1/Controller/LeagueVC.swift
@@ -9,18 +9,42 @@
 import UIKit
 
 class LeagueVC: UIViewController {
+    
+    var player:Player!
 
+    @IBOutlet weak var nextBtn: BorderButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        player = Player();
 
-        // Do any additional setup after loading the view.
     }
     
+    @IBAction func onCoedTapped(_ sender: Any) {
+        selectLeague(leagueType:"coed")
 
+    }
+    @IBAction func onWomensTapped(_ sender: Any) {
+        selectLeague(leagueType:"womens")
+
+
+    }
+    
+    @IBAction func onMensTapped(_ sender: Any) {
+        selectLeague(leagueType:"mens")
+
+    }
     
     @IBAction func onNextTapped(_ sender: Any) {
         performSegue(withIdentifier: "skillVCSegue", sender: self)
         
+        
+    }
+    
+    func selectLeague(leagueType:String){
+        player.desiredLeague = leagueType
+        nextBtn.isEnabled = true
+
         
     }
     
---
+++ SwiftPractice1/SwiftPractice1/Model/player.swift
@@ -0,0 +1,15 @@
+//
+//  player.swift
+//  SwiftPractice1
+//
+//  Created by nitish on 16/05/19.
+//  Copyright © 2019 nitish. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague:String?
+    var selectedSkillLevel:String?
+    
+}
